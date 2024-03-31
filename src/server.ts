aa93496f132d975f8a9e593a54b06bd08314460e
Matheus Lucas <m7hsouza@gmail.com>
2024-03-31 11:44:01 -0400

    Update server port configuration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from './app'
+import { env } from './config/env'
 
-app.listen({ port: 3000 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running on http://localhost:3000')
 })
