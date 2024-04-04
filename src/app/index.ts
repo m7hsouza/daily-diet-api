0325e79966fc65ddbbb541f31b0dc42b39abe6dc
Matheus Lucas <m7hsouza@gmail.com.br>
2024-04-04 08:26:33 -0400

    Adiciona rota de atualização de refeição

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,7 @@ import cookie from '@fastify/cookie'
 import { createMealRoute } from './route/create-meal'
 import { listMealsRoute } from './route/list-meals'
 import { showMealRoute } from './route/show-meal'
+import { updateMealRoute } from './route/update-meal'
 
 const app = fastify()
 
@@ -12,5 +13,6 @@ app.register(cookie)
 app.register(createMealRoute)
 app.register(listMealsRoute)
 app.register(showMealRoute)
+app.register(updateMealRoute)
 
 export { app }
---
+++ src/app/route/update-meal.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+import { FastifyInstance } from 'fastify'
+
+import { knex } from '../../config/database'
+import { checkIfExistsSessionId } from '../middleware/check-if-exists-session-id'
+
+export const updateMealRoute = async (app: FastifyInstance) => {
+  app
+    .addHook('preHandler', checkIfExistsSessionId)
+    .put('/meals/:id', async (request, reply) => {
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        meal_datetime: z.string().datetime(),
+        in_diet: z.boolean(),
+      })
+      const updateMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const body = updateMealBodySchema.parse(request.body)
+      const { id } = updateMealParamsSchema.parse(request.params)
+
+      await knex('meals')
+        .where({
+          id,
+          session_id: request.cookies.sessionId,
+        })
+        .update(body)
+
+      return reply.status(204).send()
+    })
+}
